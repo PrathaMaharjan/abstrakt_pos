6d7fa2966991b8704cbcbf6c83b9f77969806fac
PrathaMaharjan <pratha.maharjan03@gmail.com>
2026-06-11 17:25:14 +0545

    connected backend and frontend for tenant registration and login

--- drizzle.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  schema: "./src/db/schema/*", // ← wildcard to pick up all files in the folder
-  out: "./drizzle",
-  dialect: "postgresql",
-  dbCredentials: { url: process.env.DATABASE_URL! },
-});
